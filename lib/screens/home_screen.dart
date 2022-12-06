0dc73025b4f996072f8c62fb6de2b5e940781764
Jingurah <jingurah@gmail.com>
2022-12-06 03:17:01 +0300

    yey yey added ticket_view

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,9 @@
+import 'package:daths_flights/screens/ticket_view.dart';
+import 'package:fluentui_icons/fluentui_icons.dart';
 import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
+
+import '../utils/app_styles.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -6,37 +11,68 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: Color(0xFFeeedf2),
+        backgroundColor: Styles.bgColor,
         body: ListView(children: [
           Container(
+              padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
               child: Column(children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: const [
-                      Text(
-                        "Daths Flights",
-                      ),
-                      Text(
-                        "Book Tickets",
-                      ),
-                    ]),
+                const Gap(40),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            "Daths Flights",
+                            style: Styles.headLineStyle3,
+                          ),
+                          const Gap(5),
+                          Text(
+                            "Book Tickets",
+                            style: Styles.headLineStyle,
+                          ),
+                        ]),
+                    Container(
+                      height: 50,
+                      width: 50,
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10),
+                          image: const DecorationImage(
+                              fit: BoxFit.fitHeight,
+                              image: AssetImage(
+                                "assets/images/img_1.png",
+                              ))),
+                    )
+                  ],
+                ),
+                const Gap(25),
                 Container(
-                  height: 50,
-                  width: 50,
-                  decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(10),
-                      image: const DecorationImage(
-                          fit: BoxFit.fitHeight,
-                          image: AssetImage(
-                            "assets/images/img_1.png",
-                          ))),
-                )
-              ],
-            )
-          ]))
+                    decoration: BoxDecoration(
+                        color: const Color(0xFFF4F6FD),
+                        borderRadius: BorderRadius.circular(10)),
+                    padding: const EdgeInsets.symmetric(horizontal: 12),
+                    child: Row(children: [
+                      const Icon(FluentSystemIcons.ic_fluent_search_regular,
+                          color: Color(0xFFBFC205)),
+                      Text(
+                        "Search",
+                        style: Styles.headLineStyle4,
+                      )
+                    ])),
+                const Gap(40),
+                Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text("Upcoming Flights", style: Styles.headLineStyle2),
+                      InkWell(
+                          onTap: () {},
+                          child: Text("View All",
+                              style: Styles.textStyle
+                                  .copyWith(color: Styles.primaryColor)))
+                    ])
+              ])),
+          TicketView(),
         ]));
   }
 }
